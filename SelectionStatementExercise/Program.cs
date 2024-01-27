149b5d9ce88477348e87750e48887f393c2c0211
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-01-27 15:03:40 -0500

    redo complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favNumber = 14;
+            bool guessedCorrectly = false;
+
+
+
+            while (!guessedCorrectly)
+            {
+                Console.WriteLine("Guess my favorite number:");
+                var userGuess = int.Parse(Console.ReadLine());
+
+                if (userGuess < favNumber)
+                {
+                    Console.WriteLine("Sorry, the number you guessed is too low. Try again!");
+                }
+                else if (userGuess > favNumber)
+                {
+                    Console.WriteLine("Sorry, the number you guessed is too high. Try again!");
+                }
+                else
+                {
+                    Console.WriteLine("Congrats! You have correctly guessed my favorite number.");
+                    guessedCorrectly = true;
+                }
+            }
         }
     }
 }
